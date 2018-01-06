23cf46970600868c84712cc973d4ada2af9c3907
gefangshuai <gefangshuai@163.com>
2018-01-06 16:40:44 +0800

    coding

--- src/components/one/Answer.vue
+++ src/components/one/Answer.vue
@@ -14,16 +14,19 @@
         <div class="site-title">
             满意吧餐厅-餐后满意度
         </div>
-        <score-answer></score-answer>
+        <!--<score-answer></score-answer>-->
+        <select-answer></select-answer>
     </div>
 </template>
 <script>
     import ScoreAnswer from './partial/ScoreAnswer'
+    import SelectAnswer from './partial/SelectAnswer'
     import {mapGetters} from 'vuex';
 
     export default {
         components: {
-            ScoreAnswer
+            ScoreAnswer,
+            SelectAnswer
         },
         data(){
             return {
---
+++ src/components/one/partial/SelectAnswer.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="answer-content answer-select">
+        <div class="title">
+            您在消费过程中，是否遇到下面情况？
+            <div class="sub-title">
+                (多选题，最多选择3个)
+            </div>
+        </div>
+        <div class="body">
+            <mt-checklist
+                class="select"
+                v-model="value"
+                :options="options">
+            </mt-checklist>
+            <div class="border"></div>
+            <div class="action-btns">
+                <mt-button type="default" class="left" @click="$router.go(-1)">
+                    <i class="iconfont icon-back"></i>
+                    返回
+                </mt-button>
+                <mt-button type="primary" class="right">
+                    下一题
+                    <i class="iconfont icon-more"></i>
+                </mt-button>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    import $ from "jquery";
+    export default {
+        name: 'SelectAnswer',
+        data() {
+            return {
+                value: [],
+                options: [
+                    '未招呼领座', '未及时送菜', '未倒水', '为准备好餐具', '未及时送菜单'
+                ]
+            }
+        },
+        watch: {
+            value() {
+                $('.mint-checklist-label').removeClass('checked');
+                $('input.mint-checkbox-input:checked').closest('.mint-checklist-label').addClass('checked')
+            }
+        }
+    }
+</script>
